c970af2cc52a26a6a6c3555d0b4eeb6b54f3ae47
yhh <yhh@DESKTOP-9422D2K>
2020-06-14 19:06:11 +0800

    第二次提交。。。。发生大发大

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    public function text(){
+        dd(111);die;
+    }
 }
